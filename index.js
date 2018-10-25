471a217f680b8da569019310bd318371c3ed7146
Hale III, Edward <edward.hale@tyson.com>
2018-10-25 10:11:34 -0500

    Added ejs, os node modules. Pass hostname to html file for output

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //Load express module with 'require' directive
 var express = require('express')
+var os = require('os')
+var hostname = os.hostname()
 var app = express()
+app.set('view engine', 'ejs')
 
 //Define request response in root URL (/)
 app.get('/', function(req, res) {
-	res.send('Hello World!')
+	res.render('index', { hostname: hostname })
 })
 
 //Launch listening server on port 8081
