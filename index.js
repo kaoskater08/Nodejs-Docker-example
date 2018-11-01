10c18dfe6362eb44bd63e68d47e968fdba0a68e5
Hale III, Edward <edward.hale@tyson.com>
2018-11-01 13:27:47 -0500

    Added semi-colons to code

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// Load express module with 'require' directive
-var express = require('express')
-var os = require('os')
+// Load express and os modules with 'require' directive
+var express = require('express');
+var os = require('os');
 
 // Set variables
-var hostname = os.hostname()
-var app = express()
-app.set('view engine', 'ejs')
+var hostname = os.hostname();
+var port = 8081;
+var app = express();
+app.set('view engine', 'ejs');
 
 // Define request response in root URL (/)
 app.get('/', function(req, res) {
-	res.render('index', { hostname: hostname })
-})
+	res.render('index', { hostname: hostname });
+});
 
 // Launch listening server on port 8081
-app.listen(8081, function() {
-	console.log('app listening on port 8081!')
-})
+app.listen(port, function() {
+	console.log('app listening on port ${port}!');
+});
